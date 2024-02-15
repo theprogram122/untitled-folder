90cc6f7e60a61a6ec4b6277fd543fad0b39b6abc
theprogram122 <157767328+theprogram122@users.noreply.github.com>
2024-02-15 17:59:27 -0600

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,7 @@
+// valor aleatorio generado
+  
+// contar el número de intentos
+// crear para el intento correcto
+
+  
+// función para el número adivinado por el usuario     
---
+++ login.js
@@ -0,0 +1,7 @@
+function Login() {
+
+    player_name = document.getElementById("playername").value;
+    localStorage.setItem("player_name", player_name);
+
+    window.location = "gamepage.html";
+}
